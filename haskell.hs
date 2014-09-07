eadd7922257e9461de64102fce97a304340ac988
Adam Grandén <adam.granden@hotmail.com>
2014-09-07 12:07:25 +0200

    Update haskell.hs
    New update version with assigment 4.To do check it

--- haskell.hs
+++ haskell.hs
@@ -1,4 +1,13 @@
-	-- assigment 1: 
+
+-- assigment 1: 
+--In order to calculate "power n k", for a given n and k, how many computing "steps" are being used?
+--Hint: "power n 0" takes 1 step.
+--"power n 1" takes 1 step, and then uses "power n 0".
+--"power n 2" takes 1 step, and then uses "power n 1".
+--"power n 3" takes 1 step, and then uses "power n 2".
+--And so forth.
+--Write your answer as a comment in the Haskell file you submit. 
+
 power :: Integer -> Integer -> Integer 
 power n k | k < 0 = error "power: negative argument"
 power n 0 = 1
@@ -23,17 +32,17 @@ power2 n k
 
 	--assigment 4
 --a)
-	--
-	
-
+	-- To see if the statments are true or not we have to check for some values which are not in the Range of the two functions such as if it bettween  1 to 100.Might also check for the value of 0 and something which is outside the value of the list such as checking for the value of -1000.For the value of -1000 should give that the value is not the same.
+		
+		
 --b)
 comparePower1:: Integer->Integer->Bool -- ComparePower1 takes values of n k as an interger and a bool which is power and power1 check.
-comparePower1 n k= power n k == power1 n k --
+comparePower1 n k= power n k == power1 n k -- the funcation of the whole statement checks if 
 		
 comparePower2::Integer-> Integer-> Bool --ComparePower2 takes values of n k as an interger and a bool which is power and power1 check.
 comparePower2 n k = power1 n k == power2 n k -- the funcations of the whole statement which checks the power1 n k and power2 n k 
 
 --c)
-testPowerFunctions::Bool-- testpowerfunctions takes an bool to check if the value of the two statements are the same 
-testPowerFunctions = and[comparePower1 n k == comparePower2 n k -- The and function is the boolean fucntion that checks if the satement for function comparepower1 n k is the same for caomparePower2 n k 
+testAllPowerFunctions::Bool-- testpowerfunctions takes an bool to check if the value of the two statements are the same 
+testAllPowerFunctons = and[comparePower1 n k == comparePower2 n k -- The and function is the boolean fucntion that checks if the satement for function comparepower1 n k is the same for caomparePower2 n k 
  | k<- [0,10..100],n<-[-100,-90..100]] -- the guard then checks if the k is between 0 to 100 and n is between the value of -100 to 100.
